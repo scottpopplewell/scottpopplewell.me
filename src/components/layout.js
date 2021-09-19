9efd715df02e32450b7345838e531174cba95332
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-09-18 21:09:23 -0500

    feat: adds programatic recipe pages
    Uses the File System Route API + graphwl to dynamic create recipe pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,7 @@ const Layout = ({ pageTitle, children }) => {
 
   return (
     <div className={container}>
-      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <title>{data.site.siteMetadata.title}</title>
       <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
---
+++ src/pages/cookbook/{mdx.slug}.js
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx' // highlight-line
+import Layout from '../../components/layout'
+
+const Recipe = ({ data }) => {
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <MDXRenderer>
+        {data.mdx.body}
+      </MDXRenderer>
+    </Layout>
+  )
+}
+
+export default Recipe
+
+// highlight-start
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`
