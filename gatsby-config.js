f5807d0cbdcfc94d53cc5d93f6fb6433da692943
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-09-18 20:21:38 -0500

    feat: adds cookbook data example

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.scottpopplewell.me",
     title: "Scott's Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `cookbook`,
+        path: `${__dirname}/cookbook`,
+      }
+    }
   ],
 };
