99f2c11c7e0a52f232fb636bc990393b5c03e1ca
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-10-30 14:26:10 -0500

    feat: initial blog

---
+++ content/blog/first-post/index.md
@@ -0,0 +1,13 @@
+---
+title: First Post!
+date: "2021-10-30T19:19:12+00:00"
+description: "Saving time"
+---
+
+This is my first post on my new blog! How exciting!
+
+I am a software engineer born and raised in Dallas, Tx with a brief stint in the wonderful, Charlottesville, Virginia. While I absolutely could write a custom blog site in Angular/React/Lit/etc, I would love to [save time and focus on the important things.](https://twitter.com/kelseyhightower/status/1022837097743319040?lang=en)
+
+This blog was built using Kyle Mathews's template. Kyle created Gatsby.
+
+You can find the source for this blog at https://github.com/scottpopplewell/scottpopplewell.me
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,39 +1,36 @@
-import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-} from './layout.module.css'
+import * as React from "react"
+import { Link } from "gatsby"
 
-const Layout = ({ pageTitle, children }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <Link className="header-link-home" to="/">
+        {title}
+      </Link>
+    )
+  }
 
   return (
-    <div className={container}>
-      <title>{data.site.siteMetadata.title}</title>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
-          <li className={navLinkItem}><Link className={navLinkText} to="/uses">Uses</Link></li>
-          <li className={navLinkItem}><Link to="/cookbook" className={navLinkText}>Cookbook</Link></li>
-        </ul>
-      </nav>
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
     </div>
   )
 }
+
 export default Layout
