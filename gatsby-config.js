9fb8ca0989714b289efe9e809309efac6d67f76b
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-10-30 14:56:59 -0500

    Revert "fix: removes favicon"
    This reverts commit 650241db879b15fb78435d57a45be30b82d664d0.

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,7 +120,8 @@ module.exports = {
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui`
+        display: `minimal-ui`,
+        icon: `static/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
