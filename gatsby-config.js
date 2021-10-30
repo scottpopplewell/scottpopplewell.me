650241db879b15fb78435d57a45be30b82d664d0
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-10-30 14:54:41 -0500

    fix: removes favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,8 +120,7 @@ module.exports = {
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `static/favicon.ico`, // This path is relative to the root of the site.
+        display: `minimal-ui`
       },
     },
     `gatsby-plugin-react-helmet`,
