56070f8a928b771e1a61bc27e7ea5865af0eddce
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-11-19 15:57:18 -0600

    fix: removes slug creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,4 @@
 const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
@@ -55,20 +54,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           nextPostId: nextId
         },
       })
-    })
-  }
-}
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
     })
   }
 }
